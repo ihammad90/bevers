9a91c5a507630e69d4b648dc4dc5390ca50567dd
ihammad90 <67440951+ihammad90@users.noreply.github.com>
2024-01-07 22:05:33 +1300

    Update tfidf.py

--- src/data/tfidf.py
+++ src/data/tfidf.py
@@ -269,8 +269,7 @@ def generate_title_tfidf_features(args):
         norm=args.norm,
         strip_accents=args.strip_accents,
         token_pattern=r"(?u)\b\w+\b",
-        sublinear_tf=args.sublinear_tf,
-        standard_idf=True,
+        sublinear_tf=args.sublinear_tf
     )
     title_vectorizer.fit(cleaned_titles)
     if args.save:
@@ -303,8 +302,7 @@ def generate_document_tfidf_features(args):
         norm=args.norm,
         strip_accents=args.strip_accents,
         token_pattern=r"(?u)\b\w+\b",
-        sublinear_tf=args.sublinear_tf,
-        standard_idf=True,
+        sublinear_tf=args.sublinear_tf
     )
     document_vectorizer.fit(texts)
     if args.save:
@@ -343,8 +341,7 @@ def generate_tfidf_features(args):
         norm=args.norm,
         strip_accents=args.strip_accents,
         token_pattern=r"(?u)\b\w+\b",
-        sublinear_tf=args.sublinear_tf,
-        standard_idf=True,
+        sublinear_tf=args.sublinear_tf
     )
     concat_vectorizer.fit(concat_documents)
     del concat_documents
